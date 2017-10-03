d2cdb093ad7ed3b94fdfe7517b29718540300b48
sidewinder2020 <sdwinder1@gmail.com>
2017-10-03 17:17:38 -0600

    Create Transaction search and random controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,33 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
       namespace :merchants do
         get "find", to: "search#show"
         get "find_all", to: "search#index"
         get "random", to: "random#show"
       end
+      resources :merchants, only: [:index, :show]
 
-      resources :invoices, only: [:index, :show]
       namespace :invoices do
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
         get "random", to: "random#show"
       end
+      resources :invoices, only: [:index, :show]
 
-      resources :customers, only: [:index, :show]
       namespace :customers do
         get "find", to: "search#show"
         get "find_all", to: "search#index"
         get "random", to: "random#show"
       end
+      resources :customers, only: [:index, :show]
+
+      namespace :transactions do
+        get "find", to: "search#show"
+        get "find_all", to: "search#index"
+        get "random", to: "random#show"
+      end
+      resources :transactions, only: [:index, :show]
     end
   end
 end
--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -11,125 +11,148 @@ describe "Transactions API" do
     expect(transactions.count).to eq(5)
   end
 
-#   it "can grab one customer by id" do
-#     id = create(:customer).id
-#
-#     get "/api/v1/customers/#{id}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer["id"]).to eq(id)
-#   end
-#
-#   it "can search for customer by name" do
-#     first_name = create(:customer).first_name
-#
-#     get "/api/v1/customers/find?first_name=#{first_name}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer["first_name"]).to eq(first_name)
-#   end
-#
-#   it "can search for customer by last_name" do
-#     last_name = create(:customer).last_name
-#
-#     get "/api/v1/customers/find?last_name=#{last_name}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer["last_name"]).to eq(last_name)
-#   end
-#
-#   it "can search for customer by created_at" do
-#     customer = create(:customer)
-#     id = customer.id
-#
-#     get "/api/v1/customers/find?created_at=#{customer.created_at}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer["id"]).to eq(id)
-#   end
-#
-#   it "can search for customer by updated_at" do
-#     customer = create(:customer)
-#     id = customer.id
-#
-#     get "/api/v1/customers/find?updated_at=#{customer.updated_at}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer["id"]).to eq(id)
-#   end
-#
-#   it "can get all customers by matching id" do
-#     id = create(:customer).id
-#
-#     get "/api/v1/customers/find_all?id=#{id}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer.first["id"]).to eq(id)
-#     expect(customer.count).to eq(1)
-#   end
-#
-#   it "can get all customers by matching first_name" do
-#     first_name = create(:customer).first_name
-#
-#     get "/api/v1/customers/find_all?first_name=#{first_name}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer.first["first_name"]).to eq(first_name)
-#     expect(customer.count).to eq(1)
-#   end
-#
-#   it "can get all customers by matching last_name" do
-#     last_name = create(:customer).last_name
-#
-#     get "/api/v1/customers/find_all?last_name=#{last_name}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer.first["last_name"]).to eq(last_name)
-#     expect(customer.count).to eq(1)
-#   end
-#
-#   it "can get all customers by matching updated_at" do
-#     customer = create(:customer)
-#     id = customer.id
-#
-#     get "/api/v1/customers/find_all?updated_at=#{customer.updated_at}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer.first["id"]).to eq(id)
-#     expect(customer.count).to eq(1)
-#   end
-#
-#   it "can get all customers by matching created_at" do
-#     customer = create(:customer)
-#     id = customer.id
-#
-#     get "/api/v1/customers/find_all?created_at=#{customer.created_at}"
-#
-#     customer = JSON.parse(response.body)
-#
-#     expect(response).to be_success
-#     expect(customer.first["id"]).to eq(id)
-#     expect(customer.count).to eq(1)
-#   end
-#
+  it "can grab one transaction by id" do
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/#{id}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["id"]).to eq(id)
+  end
+
+  it "can search for transaction by credit_card_number" do
+    credit_card_number = create(:transaction).credit_card_number
+
+    get "/api/v1/transactions/find?credit_card_number=#{credit_card_number}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["credit_card_number"]).to eq(credit_card_number)
+  end
+
+  it "can search for transaction by result" do
+    result = create(:transaction).result
+
+    get "/api/v1/transactions/find?result=#{result}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["result"]).to eq(result)
+  end
+
+  it "can search for transaction by invoice_id" do
+    invoice_id = create(:transaction).invoice_id
+
+    get "/api/v1/transactions/find?invoice_id=#{invoice_id}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["invoice_id"]).to eq(invoice_id)
+  end
+
+  it "can search for transaction by created_at" do
+    transaction = create(:transaction)
+    id = transaction.id
+
+    get "/api/v1/transactions/find?created_at=#{transaction.created_at}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["id"]).to eq(id)
+  end
+
+  it "can search for transaction by updated_at" do
+    transaction = create(:transaction)
+    id = transaction.id
+
+    get "/api/v1/transactions/find?updated_at=#{transaction.updated_at}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction["id"]).to eq(id)
+  end
+
+  it "can get all transactions by matching id" do
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/find_all?id=#{id}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction.first["id"]).to eq(id)
+    expect(transaction.count).to eq(1)
+  end
+
+  it "can get all transactions by matching credit_card_number" do
+    credit_card_number = create(:transaction).credit_card_number
+
+    get "/api/v1/transactions/find_all?credit_card_number=#{credit_card_number}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction.first["credit_card_number"]).to eq(credit_card_number)
+    expect(transaction.count).to eq(1)
+  end
+
+  it "can get all transactions by matching invoice_id" do
+    invoice_id = create(:transaction).invoice_id
+
+    get "/api/v1/transactions/find_all?invoice_id=#{invoice_id}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction.first["invoice_id"]).to eq(invoice_id)
+    expect(transaction.count).to eq(1)
+  end
+
+  it "can get all transactions by matching result" do
+    result = create(:transaction).result
+
+    get "/api/v1/transactions/find_all?result=#{result}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction.first["result"]).to eq(result)
+    expect(transaction.count).to eq(1)
+  end
+
+  it "can get all transactions by matching updated_at" do
+    transaction = create(:transaction)
+    id = transaction.id
+
+    get "/api/v1/transactions/find_all?updated_at=#{transaction.updated_at}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction.first["id"]).to eq(id)
+    expect(transaction.count).to eq(1)
+  end
+
+  it "can get all transactions by matching created_at" do
+    transaction = create(:transaction)
+    id = transaction.id
+
+    get "/api/v1/transactions/find_all?created_at=#{transaction.created_at}"
+
+    transaction = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transaction.first["id"]).to eq(id)
+    expect(transaction.count).to eq(1)
+  end
+
 #   # it "can pull a random record" do
 #   #   create_list(:merchant, 5)
 #   #
