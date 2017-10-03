910572ee6e9abcb614bb9717366764a4cac2cd61
sidewinder2020 <sdwinder1@gmail.com>
2017-10-02 19:52:56 -0600

    Create controller and initial test for merchants

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class MerchantsController < ApplicationController
+
+      def index
+      end
+      
+    end
+  end
+end
---
+++ spec/factories/merchants.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :merchant do
+    name "Skeleton Sal"
+  end
+end
