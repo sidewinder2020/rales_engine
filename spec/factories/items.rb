d11b3d9741b772e69086151ecfcaf35759287f88
JunePaloma <kborkoski@gmail.com>
2017-10-03 14:39:40 -0600

    Add factories for transactions items customers

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :item do
+    name "Skeleton"
+    description "Bones"
+    unit_price 10000
+    merchant_id 1
+    created_at "2012-03-27 14:53:59 UTC"
+    updated_at "2012-03-28 14:53:59 UTC"
+  end
+end
