6a87c9bbbbc833888948d32240ce7d550aeb40d1
JunePaloma <kborkoski@gmail.com>
2017-10-03 15:40:53 -0600

    stage for merge to master

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :transaction do
-    invoice_id 1
+
     credit_card_number 4654405418249632
     result "success"
     credit_card_expiratio_date nil
     created_at "2012-03-27 14:53:59 UTC"
     updated_at "2012-03-28 14:53:59 UTC"
+
+  invoice
   end
 end
-Add Comment
