3a5f6ec1bb745c382c2a93716969ba0720b832e1
sidewinder2020 <sdwinder1@gmail.com>
2017-10-02 19:37:46 -0600

    Fix syntax in rails_helper and factory_girl - rspec functioning

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of merchants" do
+    create_list(:merchant, 5)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_success
+  end
+end
