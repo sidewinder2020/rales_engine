b007e78d75de072dc03ddf81259f49546b5b55f8
sidewinder2020 <sdwinder1@gmail.com>
2017-10-03 18:11:42 -0600

    Create Random Method, Finish Random Controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def random
+    order("random()").first
+  end
 end
