2e114362c12290f79d87c2135258c80da3d1345a
sidewinder2020 <sdwinder1@gmail.com>
2017-10-03 18:51:05 -0600

    Complete tests on Invoice Items, Finish Search Controller, only random left

---
+++ app/controllers/api/v1/invoice_items/search_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    module InvoiceItems
+      class SearchController < ApplicationController
+
+        def index
+          render json: InvoiceItem.where(search_params)
+        end
+
+        def show
+          render json: InvoiceItem.find_by(search_params)
+        end
+
+        private
+
+        def search_params
+          params.permit(:id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at)
+        end
+      end
+    end
+  end
+end
--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -11,170 +11,170 @@ describe "Invoice Items API" do
     expect(invoice_items.count).to eq(5)
   end
 
-  # it "can grab one item by id" do
-  #   id = create(:item).id
-  #
-  #   get "/api/v1/items/#{id}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["id"]).to eq(id)
-  # end
-  #
-  # it "can search for item by name" do
-  #   name = create(:item).name
-  #
-  #   get "/api/v1/items/find?name=#{name}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["name"]).to eq(name)
-  # end
-  #
-  # it "can search for item by description" do
-  #   description = create(:item).description
-  #
-  #   get "/api/v1/items/find?description=#{description}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["description"]).to eq(description)
-  # end
-  #
-  # it "can search for item by unit_price" do
-  #   unit_price = create(:item).unit_price
-  #
-  #   get "/api/v1/items/find?unit_price=#{unit_price}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["unit_price"]).to eq(unit_price)
-  # end
-  #
-  # it "can search for item by merchant_id" do
-  #   merchant_id = create(:item).merchant_id
-  #
-  #   get "/api/v1/items/find?merchant_id=#{merchant_id}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["merchant_id"]).to eq(merchant_id)
-  # end
-  #
-  # it "can search for item by created_at" do
-  #   item = create(:item)
-  #   id = item.id
-  #
-  #   get "/api/v1/items/find?created_at=#{item.created_at}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["id"]).to eq(id)
-  # end
-  #
-  # it "can search for item by updated_at" do
-  #   item = create(:item)
-  #   id = item.id
-  #
-  #   get "/api/v1/items/find?updated_at=#{item.updated_at}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item["id"]).to eq(id)
-  # end
-  #
-  # it "can get all items by matching id" do
-  #   id = create(:item).id
-  #
-  #   get "/api/v1/items/find_all?id=#{id}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["id"]).to eq(id)
-  #   expect(item.count).to eq(1)
-  # end
-  #
-  # it "can get all items by matching name" do
-  #   name = create(:item).name
-  #
-  #   get "/api/v1/items/find_all?name=#{name}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["name"]).to eq(name)
-  #   expect(item.count).to eq(1)
-  # end
-  #
-  # it "can get all items by matching description" do
-  #   description = create(:item).description
-  #
-  #   get "/api/v1/items/find_all?description=#{description}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["description"]).to eq(description)
-  #   expect(item.count).to eq(1)
-  # end
-  #
-  # it "can get all items by matching unit_price" do
-  #   unit_price = create(:item).unit_price
-  #
-  #   get "/api/v1/items/find_all?unit_price=#{unit_price}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["unit_price"]).to eq(unit_price)
-  #   expect(item.count).to eq(1)
-  # end
-  #
-  # it "can get all items by matching merchant_id" do
-  #   merchant_id = create(:item).merchant_id
-  #
-  #   get "/api/v1/items/find_all?merchant_id=#{merchant_id}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["merchant_id"]).to eq(merchant_id)
-  #   expect(item.count).to eq(1)
-  # end
-  #
-  # it "can get all items by matching updated_at" do
-  #   item = create(:item)
-  #   id = item.id
-  #
-  #   get "/api/v1/items/find_all?updated_at=#{item.updated_at}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["id"]).to eq(id)
-  #   expect(item.count).to eq(1)
-  # end
-  #
-  # it "can get all items by matching created_at" do
-  #   item = create(:item)
-  #   id = item.id
-  #
-  #   get "/api/v1/items/find_all?created_at=#{item.created_at}"
-  #
-  #   item = JSON.parse(response.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.first["id"]).to eq(id)
-  #   expect(item.count).to eq(1)
-  # end
+  it "can grab one invoice_item by id" do
+    id = create(:invoice_item).id
+
+    get "/api/v1/invoice_items/#{id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(id)
+  end
+
+  it "can search for invoice_item by item_id" do
+    item_id = create(:invoice_item).item_id
+
+    get "/api/v1/invoice_items/find?item_id=#{item_id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["item_id"]).to eq(item_id)
+  end
+
+  it "can search for invoice_item by invoice_id" do
+    invoice_id = create(:invoice_item).invoice_id
+
+    get "/api/v1/invoice_items/find?invoice_id=#{invoice_id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["invoice_id"]).to eq(invoice_id)
+  end
+
+  it "can search for invoice_item by quantity" do
+    quantity = create(:invoice_item).quantity
+
+    get "/api/v1/invoice_items/find?quantity=#{quantity}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["quantity"]).to eq(quantity)
+  end
+
+  it "can search for invoice_item by unit_price" do
+    unit_price = create(:invoice_item).unit_price
+
+    get "/api/v1/invoice_items/find?unit_price=#{unit_price}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["unit_price"]).to eq(unit_price)
+  end
+
+  it "can search for invoice_item by created_at" do
+    invoice_item = create(:invoice_item)
+    id = invoice_item.id
+
+    get "/api/v1/invoice_items/find?created_at=#{invoice_item.created_at}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(id)
+  end
+
+  it "can search for invoice_item by updated_at" do
+    invoice_item = create(:invoice_item)
+    id = invoice_item.id
+
+    get "/api/v1/invoice_items/find?updated_at=#{invoice_item.updated_at}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(id)
+  end
+
+  it "can get all invoice_items by matching id" do
+    id = create(:invoice_item).id
+
+    get "/api/v1/invoice_items/find_all?id=#{id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["id"]).to eq(id)
+    expect(invoice_item.count).to eq(1)
+  end
+
+  it "can get all invoice_items by matching item_id" do
+    item_id = create(:invoice_item).item_id
+
+    get "/api/v1/invoice_items/find_all?item_id=#{item_id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["item_id"]).to eq(item_id)
+    expect(invoice_item.count).to eq(1)
+  end
+
+  it "can get all invoice_items by matching invoice_id" do
+    invoice_id = create(:invoice_item).invoice_id
+
+    get "/api/v1/invoice_items/find_all?invoice_id=#{invoice_id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["invoice_id"]).to eq(invoice_id)
+    expect(invoice_item.count).to eq(1)
+  end
+
+  it "can get all invoice_items by matching unit_price" do
+    unit_price = create(:invoice_item).unit_price
+
+    get "/api/v1/invoice_items/find_all?unit_price=#{unit_price}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["unit_price"]).to eq(unit_price)
+    expect(invoice_item.count).to eq(1)
+  end
+
+  it "can get all invoice_items by matching quantity" do
+    quantity = create(:invoice_item).quantity
+
+    get "/api/v1/invoice_items/find_all?quantity=#{quantity}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["quantity"]).to eq(quantity)
+    expect(invoice_item.count).to eq(1)
+  end
+
+  it "can get all invoice_items by matching updated_at" do
+    invoice_item = create(:invoice_item)
+    id = invoice_item.id
+
+    get "/api/v1/invoice_items/find_all?updated_at=#{invoice_item.updated_at}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["id"]).to eq(id)
+    expect(invoice_item.count).to eq(1)
+  end
+
+  it "can get all invoice_items by matching created_at" do
+    invoice_item = create(:invoice_item)
+    id = invoice_item.id
+
+    get "/api/v1/invoice_items/find_all?created_at=#{invoice_item.created_at}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item.first["id"]).to eq(id)
+    expect(invoice_item.count).to eq(1)
+  end
 
   # it "can pull a random record" do
   #   create_list(:merchant, 5)
