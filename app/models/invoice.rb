0c026e10e06dc34bc4cfeb2e9e2f0181930f93c7
sidewinder2020 <sdwinder1@gmail.com>
2017-10-03 21:00:02 -0600

    total revenue

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,13 @@ class Invoice < ApplicationRecord
   def self.random
     order("random()").first
   end
+
+  def total_revenue
+    Invoice.joins("INNER JOIN transactions ON transactions.invoice_id = invoices.id AND transactions.result = 'success'")
+    .joins("INNER JOIN merchants ON merchants.id = invoices.merchant_id AND merchants.id = 1")
+    .joins("INNER JOIN invoice_items ON invoices.id = invoice_items.invoice_id")
+    .select("invoice_items.quantity * invoice_items.unit_price AS revenue")
+    .map(&:revenue)
+    .reduce(:+)
+  end
 end
