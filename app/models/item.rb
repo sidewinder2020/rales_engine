51275995897595668cd236b8b2284bd7a30c7058
sidewinder2020 <sdwinder1@gmail.com>
2017-10-04 17:33:26 -0600

    Finish person A most items and quantity

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,13 @@ class Item < ApplicationRecord
   def unit_price_to_2_sig_figs
     (unit_price / 100.0).to_s
   end
+
+  def self.most_items(limit = 5)
+    joins(invoice_items: [invoice: [:transactions]])
+    .where('transactions.result = ?', 'success')
+    .select("invoice_items.invoice_id as inv_id, count(invoice_items.invoice_id) as total")
+    .group(:inv_id)
+    .order('total DESC')
+    .limit(limit)
+  end
 end
