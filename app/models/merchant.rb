8bfabd138369e974de09b2660ddb663ab925d8f6
JunePaloma <kborkoski@gmail.com>
2017-10-04 18:49:27 -0600

    add favorite customer to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,9 +2,20 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers, through: :invoices
   has_many :items
+  has_many :invoice_items, through: :invoices
 
   def self.random
     order("random()").first
+  end
+
+  def favorite_customer
+    customers
+    .select("customers.*, count(transactions) as customer_transactions")
+    .joins(:transactions)
+    .merge(transactions.successful)
+    .group(:id)
+    .order("customer_transactions DESC")
+    .first
   end
 
 end
