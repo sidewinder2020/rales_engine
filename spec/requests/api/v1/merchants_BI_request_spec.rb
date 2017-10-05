830956223739a7a350d95600c38064997a920ca5
JunePaloma <kborkoski@gmail.com>
2017-10-05 17:13:33 -0600

    add individual merchant tests to BI request spec

--- spec/requests/api/v1/merchants_BI_request_spec.rb
+++ spec/requests/api/v1/merchants_BI_request_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "merchant business intelligence API" do
 
-  # 
+
   # it "can return the top x merchants ranked by total revenue" do
   #   get "/api/v1/merchants/most_revenue?quantity=5"
   # end
@@ -14,14 +14,46 @@ describe "merchant business intelligence API" do
   # it "can return the total revenue for date x across all merchants" do
   #   get "/api/v1/merchants/revenue?date=5"
   # end
-  #
-  # it "can return the total revenue for a merchant across successful transactions" do
-  #   get "/api/v1/merchants/#{merchant.id}/revenue"
-  # end
-  #
-  # it "can return the total revenue for a merchant for a specific invoice date" do
-  #   get "/api/v1/merchants/#{merchant.id}/revenue?date=2012-03-25%2009:54:09"
-  # end
+
+  it "can return the total revenue for a merchant across successful transactions" do
+    merchant = create(:merchant)
+    item = create(:item)
+    customer1 = create(:customer)
+    invoice1 = create(:invoice, customer: customer1, merchant: merchant)
+    invoice2 = create(:invoice, customer: customer1, merchant: merchant)
+    transaction1 = create(:transaction, invoice: invoice1)
+    transaction2 = create(:transaction, invoice: invoice1)
+    transaction3 = create(:transaction, invoice: invoice2)
+    invoice_item1 = create(:invoice_item, invoice: invoice1, item:item)
+    invoice_item2 = create(:invoice_item, invoice: invoice2, item:item)
+
+    get "/api/v1/merchants/#{merchant.id}/revenue"
+
+    revenue = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(revenue).to eq({"revenue"=>"2045.25"})
+  end
+
+  it "can return the total revenue for a merchant for a specific invoice date" do
+    merchant = create(:merchant)
+    item = create(:item)
+    customer1 = create(:customer)
+    invoice1 = create(:invoice, customer: customer1, merchant: merchant)
+    invoice2 = create(:invoice, customer: customer1, merchant: merchant)
+    transaction1 = create(:transaction, invoice: invoice1)
+    transaction2 = create(:transaction, invoice: invoice1)
+    transaction3 = create(:transaction, invoice: invoice2)
+    invoice_item1 = create(:invoice_item, invoice: invoice1, item:item)
+    invoice_item2 = create(:invoice_item, invoice: invoice2, item:item)
+
+    get "/api/v1/merchants/#{merchant.id}/revenue?date=2012-03-27%2014:53:59%20UTC"
+
+    revenue = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(revenue).to eq({"revenue"=>"2045.25"})
+  end
 
   it "can return the customer with the most successful transactions for a merchant" do
     merchant = create(:merchant)
