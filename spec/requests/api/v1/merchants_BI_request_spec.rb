4eff689f0fad05f05069528a39f768d63cab657d
JunePaloma <kborkoski@gmail.com>
2017-10-05 17:30:46 -0600

    add test for returning the top merchants by total revenue

--- spec/requests/api/v1/merchants_BI_request_spec.rb
+++ spec/requests/api/v1/merchants_BI_request_spec.rb
@@ -3,10 +3,30 @@ require "rails_helper"
 describe "merchant business intelligence API" do
 
 
-  # it "can return the top x merchants ranked by total revenue" do
-  #   get "/api/v1/merchants/most_revenue?quantity=5"
-  # end
-  #
+  it "can return the top x merchants ranked by total revenue" do
+    merchant1 = create(:merchant)
+    merchant2 = create(:merchant)
+    item = create(:item)
+    customer1 = create(:customer)
+    invoice1 = create(:invoice, customer: customer1, merchant: merchant1)
+    invoice2 = create(:invoice, customer: customer1, merchant: merchant1)
+    transaction1 = create(:transaction, invoice: invoice1)
+    transaction2 = create(:transaction, invoice: invoice1)
+    transaction3 = create(:transaction, invoice: invoice2)
+    invoice_item1 = create(:invoice_item, invoice: invoice1, item:item)
+    invoice_item2 = create(:invoice_item, invoice: invoice2, item:item)
+
+    get "/api/v1/merchants/most_revenue?quantity=1"
+
+
+    merchants = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchants.count).to eq 1
+    expect(merchants.first["id"]).to eq(merchant1.id)
+    expect(merchants.last["id"]).to_not eq(merchant2.id)
+  end
+
   it "can return the top x merchants ranked by total number of items sold" do
     merchant1 = create(:merchant)
     merchant2 = create(:merchant)
