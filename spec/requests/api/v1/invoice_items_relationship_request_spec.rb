f5055c5cd2e960443d4a5bd32f0ec434499aa1da
sidewinder2020 <sdwinder1@gmail.com>
2017-10-05 14:24:13 -0600

    invoice_items relationship tests done, spec harness passing

--- spec/requests/api/v1/invoice_items_relationship_request_spec.rb
+++ spec/requests/api/v1/invoice_items_relationship_request_spec.rb
@@ -16,9 +16,14 @@ describe "Invoice_Items Relationships API" do
 
   it "returns the associated item" do
     invoice = create(:invoice)
-    item = create(:item)
-    invoice_item = create(:invoice_item, invoice: invoice, item: item)
+    item1 = create(:item)
+    invoice_item = create(:invoice_item, invoice: invoice, item: item1)
 
     get "/api/v1/invoice_items/#{invoice.id}/item"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["id"]).to eq(item1.id)
   end
 end
